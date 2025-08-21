c7f5be430c8efe34ddeda923637833128089272b
domeeno <dominicflocea.dev@gmail.com>
2025-08-21 22:49:43 +0300

    (vimtex) configured output directory for latex compilation

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,5 +4,16 @@ return {
   init = function()
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-output-directory=build'
+      },
+    }
   end
 }
+
