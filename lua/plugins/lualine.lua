4126c1e0cf5b2ee10443c05902cb596d56aa2c34
domeeno <dominicflocea.dev@gmail.com>
2026-02-18 17:49:37 +0200

    (lualine) improved status line and tabs esthetics

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,39 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require("lualine").setup({})
+      require("lualine").setup({
+        options = {
+          theme = "tomorrow_night"
+        },
+        tabline = {
+          lualine_a = {
+            function()
+              return ""
+            end
+          },
+          lualine_b = {
+            {
+              "tabs",
+              mode = 1,
+              use_mode_colors = true,
+              path = 0,
+              symbols = { modified = "+", },
+              fmt = function(name, context)
+                local dev_icons = require("nvim-web-devicons")
+                local icon, _ = dev_icons.get_icon(name, nil, { default = true })
+                return icon .. " " .. name
+              end,
+              max_length = vim.o.columns - 1,
+            },
+
+          },
+          lualine_z = {
+            function()
+              return " " .. os.date("%H:%M")
+            end
+          },
+        }
+      })
     end
   }
 }
